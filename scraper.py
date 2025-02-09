93117cb7b4dd36b951e3e086522f445d460d329d
Tu Nguyen <tuan4@uci.edu>
2025-02-09 00:46:45 -0800

    added checking for redirections

--- scraper.py
+++ scraper.py
@@ -3,7 +3,8 @@ from urllib.parse import urlparse, urljoin, urldefrag
 from bs4 import BeautifulSoup
 
 def scraper(url, resp):
-    links = extract_next_links(url, resp)
+    final_url = resp.url
+    links = extract_next_links(final_url, resp)
     return [link for link in links if is_valid(link)]
 
 
